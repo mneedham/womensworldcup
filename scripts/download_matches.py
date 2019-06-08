c658ae5c3d82d1a9e7771601aa0a2e772efba4fa
Mark Needham <m.h.needham@gmail.com>
2019-06-08 19:49:43 +0100

    goals for knock out stages of china 2007

--- scripts/download_matches.py
+++ scripts/download_matches.py
@@ -4,6 +4,17 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
+rounds = {
+    "56336": "248548",
+    "56337": "248548",
+    "56338": "248548",
+    "56339": "248548",
+    "56335": "2000001014",
+    "56334": "2000001014",
+    "56333": "2000001015",
+    "56332": "2000001016"
+}
+
 for dir in os.listdir("data/raw/tournaments"):
     if os.path.isdir(f"data/raw/tournaments/{dir}"):
         print(dir)
@@ -18,3 +29,7 @@ for dir in os.listdir("data/raw/tournaments"):
                     if response.json():
                         with open(match_file, "w") as match_file_handle:
                             match_file_handle.write(json.dumps(response.json()))
+                    else:
+                        response = requests.get(f"https://www.fifa.com/womensworldcup/matches/round={rounds[match_id]}/match={match_id}/index.html#overview#nosticky")
+                        with open(f"data/raw/matches/{match_id}.html", "w") as match_file_handle:
+                            match_file_handle.write(response.text)
--- scripts/scrape_match_listing.py
+++ scripts/scrape_match_listing.py
@@ -1,11 +1,42 @@
 import glob
 from bs4 import BeautifulSoup
 import json
-import os
-import requests
+
+def extract_goals(soup, top_level_selector):
+    goals = []
+    for item in soup.select(f"{top_level_selector} ul.mh-l-scorers li.mh-scorer"):
+        player_id = item.select("div.p-i-no")[0]["data-player-id"]
+        for goal_element in item.select("span.ml-scorer-evmin span"):
+            if not "OG" in goal_element.text:
+                minute = goal_element.text.replace("PEN", "").strip()
+                goals.append({"IdPlayer": player_id, "Minute": minute})
+    return goals
+
+def extract_json(file):
+    document = {"SeasonShortName": [{"Description": "China 2007"}]}
+    with open(file, "r") as html_file:
+        soup = BeautifulSoup(html_file.read(), "html.parser")
+        document["IdMatch"] = soup.select("div.mh.result")[0]["data-id"]
+        document["StageName"] = [{"Description": soup.select("div.mh-i-round")[0].text}]
+        document["HomeTeam"] = {
+            "IdTeam": soup.select("div.home")[0]["data-team-id"],
+            "Score": int(soup.select("div.s-score span.s-scoreText")[0].text.split("-")[0]),
+            "Goals": extract_goals(soup, "div.t-scorer.home")
+        }
+        document["AwayTeam"] = {
+            "IdTeam": soup.select("div.away")[0]["data-team-id"],
+            "Score": int(soup.select("div.s-score span.s-scoreText")[0].text.split("-")[1]),
+            "Goals": extract_goals(soup, "div.t-scorer.away")
+        }
+    return document
 
 with open("data/matches.json", "w") as tournaments_file:
     for file in glob.glob("data/raw/matches/*.json"):
         with open(file, "r") as json_file:
             response = json_file.read()
             tournaments_file.write(f"{response}\n")
+
+    for file in glob.glob("data/raw/matches/*.html"):
+        print(file)
+        document = extract_json(file)
+        tournaments_file.write(f"{json.dumps(document)}\n")
