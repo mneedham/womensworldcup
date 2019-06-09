7f7b7e9b488cabc9d29a2d8e8fec4ce8122a500d
Mark Needham <m.h.needham@gmail.com>
2019-06-09 19:01:47 +0100

    france squads

--- scripts/download/squads.py
+++ scripts/download/squads.py
@@ -7,13 +7,26 @@ with open("data/tournaments.json", "r") as tournaments_file:
         document = json.loads(line)
         short_name = document["shortName"]
         year = document["year"]
-        for team in document["teams"]:
-            print(team)
-            tournament_dir = f"data/raw/tournaments/squads/{short_name}"
-            if not os.path.isdir(tournament_dir):
-                os.mkdir(tournament_dir)
-            location = f"{tournament_dir}/{team['id']}.html"
-            if not os.path.exists(location):
-                response = requests.get(f"https://www.fifa.com/womensworldcup/archive/edition={year}/library/teams/team={team['id']}/_players/_players_list.html")
-                with open(location, "w") as squad_file:
-                    squad_file.write(response.text)
+        print(short_name)
+        if short_name == "france2019":
+            for team in document["teams"]:
+                print(team)
+                tournament_dir = f"data/raw/tournaments/{short_name}/squads/"
+                if not os.path.isdir(tournament_dir):
+                    os.makedirs(tournament_dir, exist_ok=True)
+                location = f"{tournament_dir}/{team['id']}.html"
+                if not os.path.exists(location):
+                    response = requests.get(f"https://www.fifa.com/womensworldcup/teams/team/{team['id']}/")
+                    with open(location, "w") as squad_file:
+                        squad_file.write(response.text)
+        else:
+            for team in document["teams"]:
+                print(team)
+                tournament_dir = f"data/raw/tournaments/{short_name}/squads/"
+                if not os.path.isdir(tournament_dir):
+                    os.mkdir(tournament_dir)
+                location = f"{tournament_dir}/{team['id']}.html"
+                if not os.path.exists(location):
+                    response = requests.get(f"https://www.fifa.com/womensworldcup/archive/edition={year}/library/teams/team={team['id']}/_players/_players_list.html")
+                    with open(location, "w") as squad_file:
+                        squad_file.write(response.text)
--- scripts/scrape/squads.py
+++ scripts/scrape/squads.py
@@ -11,16 +11,31 @@ with open("data/squads.json", "w") as squads_file:
                 print(file)
                 team_id = file.split("/")[-1].replace(".html", "")
                 squad_json = {"shortName": dir, "year": int(dir[-4:]), "teamId": team_id, "players": []}
-                with open(file, "r") as fh:
-                    soup = BeautifulSoup(fh.read(), "html.parser")
-                    for player in soup.select("div.p-list div.p-i-no"):
-                        player_id = player["data-player-id"]
-                        name = player["data-player-name"]
-                        role = player.get("data-player-role", "")
+                if "france2019" in file:
+                    with open(file, "r") as fh:
+                        soup = BeautifulSoup(fh.read(), "html.parser")
+                        for player in soup.select("div.fi-team__members div.fi-p"):
+                            player_element = player.select("div.fi-p__n a")[0]
+                            player_id = player_element["href"].split("/")[-2].strip()
+                            name = player_element["title"]
+                            role = "0" if "coach" in player_element["href"] else player["data-member-pos"]
+                            dob = ""
+                            print(player_id, name, role, dob)
+                            squad_json["players"].append({"id": player_id, "name": name, "role": role, "dob": dob})
+                            # tournament_json["teams"].append({"team": team.text, "id": team["href"].split("/")[-2].replace("team=", "")})
+                    squads_file.write(f"{json.dumps(squad_json)}\n")
 
-                        dob = player.select("div.p-ag span")[0].get("data-birthdate", "")
+                else:
+                    with open(file, "r") as fh:
+                        soup = BeautifulSoup(fh.read(), "html.parser")
+                        for player in soup.select("div.p-list div.p-i-no"):
+                            player_id = player["data-player-id"]
+                            name = player["data-player-name"]
+                            role = player.get("data-player-role", "")
 
-                        print(player_id, name, role, dob)
-                        squad_json["players"].append({"id": player_id, "name": name, "role": role, "dob": dob})
-                        # tournament_json["teams"].append({"team": team.text, "id": team["href"].split("/")[-2].replace("team=", "")})
-                squads_file.write(f"{json.dumps(squad_json)}\n")
+                            dob = player.select("div.p-ag span")[0].get("data-birthdate", "")
+
+                            print(player_id, name, role, dob)
+                            squad_json["players"].append({"id": player_id, "name": name, "role": role, "dob": dob})
+                            # tournament_json["teams"].append({"team": team.text, "id": team["href"].split("/")[-2].replace("team=", "")})
+                    squads_file.write(f"{json.dumps(squad_json)}\n")
